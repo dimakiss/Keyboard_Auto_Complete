344d3d5e49db708bc84470795b577cc5e236544f
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-11-10 14:35:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import keyboard
+import keyboard,sys
 
 
 def read_shortcuts_file():
@@ -11,27 +11,45 @@ def read_shortcuts_file():
 
 def check_valid_text():
     with open("shortcuts.txt", "r+") as f:
-        old_text=f.read()
+        old_text=str(f.read())
         lines = old_text.split('\n')
 
-        new_text=lines[0] #first line
-        words =[lines[0].split(',')[0]] #add first word
+        new_text="" #first line
+        words =[] #add first word
         for line in lines:
             current_word=line.split(',')[0]
             if current_word not in words and current_word!="":
-                words.append(line.split(',')[0])
-                new_text+="\n"+line
-        if old_text!=new_text:
+                words.append(str(line.split(',')[0]))
+                new_text+=line+"\n"
+        if old_text!=new_text[:-1]:
             f.truncate(0)
-            f.write(new_text)
+            f.write(new_text[:-1])
         f.close()
 
-check_valid_text()
-shortcuts,words=read_shortcuts_file()
 
-for i in range(len(words)):
-    keyboard.register_abbreviation(shortcuts[i],words[i])
 
-while 1:
-    keyboard.wait()
+
+if __name__ == '__main__':
+
+    #sys.argv=['main.py', '-add', 'k', 'kkkk']
+    if "-add" in sys.argv:
+        if len(sys.argv) !=4 or not str(sys.argv[2]).isalpha() or not str(sys.argv[3]).isalpha():
+            print("Something went wrong make sure you use -add shortcut full_word")
+            sys.exit()
+        with open("shortcuts.txt", 'r+') as f:
+            text_to_write=str(sys.argv[2])+","+str(sys.argv[3])
+            if f.read()!="":
+                text_to_write=str(f.read())+"\n"+text_to_write
+            f.write(text_to_write)
+            f.close()
+        exit()
+
+    check_valid_text()
+    shortcuts, words = read_shortcuts_file()
+
+    for i in range(len(words)):
+        keyboard.register_abbreviation(shortcuts[i], words[i])
+
+    while 1:
+        keyboard.wait()
 
