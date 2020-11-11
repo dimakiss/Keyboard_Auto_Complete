7b0efabb555078fc04223e7b773cc1b051531218
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-11-11 03:37:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,99 @@
-import keyboard,sys
+import keyboard,sys,glob
 
+
+### CONFIG ###
+
+exceptions=[","]
+default_file_name="shortcuts.txt"
+
+### END OF CONFIG ###
 
 def read_shortcuts_file():
-    with open("shortcuts.txt",'r') as f:
-        lines=f.read().split('\n')
-        shortcuts=[i.split(',')[0] for i in lines]
-        words=[i.split(',')[1] for i in lines]
+    '''
+    Read text file content.
+    :return: The shortcuts and the full words
+    '''
+    with open(default_file_name,'r+') as f:
+        file_content=str(f.read())
+        if file_content=="":
+            print(f"The file '{default_file_name}' is empty.")
+            sys.exit()
+        lines=file_content.split('\n')
+        shortcuts=[line.split(',')[0] for line in lines]
+        words=[line.split(',')[1] for line in lines]
         f.close()
     return shortcuts,words
 
 def check_valid_text():
-    with open("shortcuts.txt", "r+") as f:
-        old_text=str(f.read())
+    '''
+    Check is the content in the text folder is valid fixes it otherwise
+    :return: None
+    '''
+    with open(default_file_name, "r+") as f:
+        old_text=f.read()
         lines = old_text.split('\n')
 
         new_text="" #first line
         words =[] #add first word
         for line in lines:
-            current_word=line.split(',')[0]
-            if current_word not in words and current_word!="":
+            current_word=str(line.split(',')[0])
+            if current_word not in words and current_word not in exceptions:
                 words.append(str(line.split(',')[0]))
                 new_text+=line+"\n"
-        if old_text!=new_text[:-1]:
-            f.truncate(0)
-            f.write(new_text[:-1])
+        f.close()
+    if old_text!=new_text[:-1]:
+        f=open(default_file_name,"w")
+        f.write(new_text[:-1])
         f.close()
 
+def is_valid_text(text):
+    '''
+    Checks if the text doesn't contain problematic characters
+    :param text: The text to check
+    :return: If the text doesn't contain problematic characters
+    '''
+    if len(text)==0:
+        return False
+    for exc in exceptions:
+        if exc in text:
+            return False
+    return True
 
+def check_if_file_exist():
+    '''
+    Checks if the text file exist in current folder
+    :return: None
+    '''
+    if not glob.glob(default_file_name):
+        print(f"Couldn't find '{default_file_name}'. Created empty file, make sure to fill it up.")
+        f=open(default_file_name,'x')
+        f.close()
+        sys.exit()
 
 
 if __name__ == '__main__':
 
-    #sys.argv=['main.py', '-add', 'k', 'kkkk']
+    check_if_file_exist()
+
     if "-add" in sys.argv:
-        if len(sys.argv) !=4 or not str(sys.argv[2]).isalpha() or not str(sys.argv[3]).isalpha():
-            print("Something went wrong make sure you use -add shortcut full_word")
+
+        if len(sys.argv) <4 or not is_valid_text(str(sys.argv[2])+str(" ".join(sys.argv[3:]))):
+            print("Something went wrong make sure you use -add [shortcut] [full_word]")
             sys.exit()
-        with open("shortcuts.txt", 'r+') as f:
-            text_to_write=str(sys.argv[2])+","+str(sys.argv[3])
+        with open(default_file_name, 'r+') as f:
+            text_to_write=str(sys.argv[2])+","+" ".join(sys.argv[3:])
             if f.read()!="":
-                text_to_write=str(f.read())+"\n"+text_to_write
+                text_to_write="\n"+text_to_write
             f.write(text_to_write)
             f.close()
+        print("'"+str(sys.argv[2])+","+" ".join(sys.argv[3:])+"' was added successfully!")
         exit()
+
 
     check_valid_text()
     shortcuts, words = read_shortcuts_file()
 
     for i in range(len(words)):
-        keyboard.register_abbreviation(shortcuts[i], words[i])
-
+        keyboard.register_abbreviation(shortcuts[i],words[i]+" ")
     while 1:
         keyboard.wait()
-
